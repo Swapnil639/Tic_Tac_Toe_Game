b0c639cd3663830c2dcab90438369df716e0fd83
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-12 10:59:30 +0530

    Added :Choose valid cells

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -9,19 +9,17 @@ public class TicTacToe {
     static TicTacToe game = new TicTacToe();
 
     public static void main(String[] args) {
-        game.gameBoard();
         game.chooseLetter();
         game.coinToss();
+        game.displayBoard();
     }
 
-    public void gameBoard() {
+    public TicTacToe() {
         board = new char[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-                System.out.print(board[i][j]);
+                board[i][j] = ' ';
             }
-            System.out.println();
         }
     }
 
@@ -45,6 +43,14 @@ public class TicTacToe {
         } else {
             System.out.println("Tails! Player 2 goes first.");
         }
+    }
+    public void displayBoard() {
+        System.out.println("   1   2   3");
+        System.out.println("1  " + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+        System.out.println("  ---+---+---");
+        System.out.println("2  " + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+        System.out.println("  ---+---+---");
+        System.out.println("3  " + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
     }
 }
 
